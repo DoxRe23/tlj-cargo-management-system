f78c55d62dca51ce7017877619c2c85a8314fc9e
DoxRe23 <oky.redian23@gmail.com>
2025-11-07 08:44:50 +0700

    upload

---
+++ application/views/backend/template/js.php
@@ -0,0 +1,168 @@
+<!-- jQuery -->
+<script src="<?php echo base_url('assets/plugins') ?>/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="<?php echo base_url('assets/plugins') ?>/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo base_url('assets/plugins') ?>/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="<?php echo base_url('assets/plugins') ?>/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="<?php echo base_url('assets/plugins') ?>/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="<?php echo base_url('assets/plugins') ?>/jqvmap/jquery.vmap.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="<?php echo base_url('assets/plugins') ?>/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="<?php echo base_url('assets/plugins') ?>/moment/moment.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="<?php echo base_url('assets/plugins') ?>/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="<?php echo base_url('assets/plugins') ?>/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="<?php echo base_url('assets/plugins') ?>/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url('assets/backend/dist') ?>/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<!-- <script src="<?php echo base_url('assets/backend/dist') ?>/js/demo.js"></script> -->
+<!--AdminLTE dashboard demo (This is only for demo purposes)-->
+<script src="<?php echo base_url('assets/backend/dist') ?>/js/pages/dashboard.js"></script>
+<!-- SweetAlert2 -->
+<script src="<?php echo base_url('assets/plugins') ?>/sweetalert2/sweetalert2.all.min.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="<?php echo base_url('assets/plugins') ?>/datatables/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/jszip/jszip.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/pdfmake/pdfmake.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/pdfmake/vfs_fonts.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="<?php echo base_url('assets/plugins') ?>/datatables-buttons/js/buttons.colVis.min.js"></script>
+
+<!-- Checkbox
+<script type="text/javascript" src="//gyrocode.github.io/jquery-datatables-checkboxes/1.2.12/js/dataTables.checkboxes.min.js"></script> -->
+
+
+<!-- myScript -->
+
+<script>
+  $(function() {
+    $("#example1")
+      .DataTable({
+        responsive: true,
+        lengthChange: false,
+        autoWidth: false,
+        buttons: ["copy", "csv", "excel", "pdf", "print", ],
+      })
+      .buttons()
+      .container()
+      .appendTo("#example1_wrapper .col-md-6:eq(0)");
+  });
+</script>
+
+
+
+<!-- 
+<script>
+  // $('#example1').DataTable({
+    scrollX: true,
+    // buttons: ["copy", "excel", "pdf", "print"],
+    // paging: true,
+
+  })
+
+
+  // .buttons()
+  // .container()
+  // .appendTo("#example1_wrapper .col-md-6:eq(0)");
+</script> -->
+
+<!-- validation form -->
+<script>
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+</script>
+
+<!-- sweetalert -->
+<script>
+  const flashData = $('.flash-data').data('flashdata');
+
+  if (flashData == "ditambahkan") {
+    Swal.fire(
+      'Berhasil!',
+      'Data Telah di Masukan',
+      'success'
+    )
+  } else if (flashData == "dikirim") {
+    Swal.fire(
+      'Berhasil!',
+      'Barang Telah di Kirim',
+      'success'
+    )
+  } else if (flashData == "gagal-kirim") {
+    Swal.fire(
+      'Gagal!',
+      'Pilih Minimal Satu Barang',
+      'error'
+    )
+  }
+
+  function confirmLogout() {
+    Swal.fire({
+      title: 'Anda Yakin Ingin Keluar?',
+      text: "",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Keluar',
+      cancelButtonText: 'Batal'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = "<?php echo base_url('auth/logout'); ?>"
+      }
+    })
+  }
+</script>
+
+<!-- sideBarFunction -->
+
+<script>
+  /** add active class and stay opened when selected */
+  var url = window.location;
+
+  // for sidebar menu entirely but not cover treeview
+  $('ul.nav-sidebar a').filter(function() {
+    return this.href == url;
+  }).addClass('active');
+
+  // for treeview
+  $('ul.nav-treeview a').filter(function() {
+    return this.href == url;
+  }).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open').prev('a').addClass('active');
+</script>
